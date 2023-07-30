5d9534a6424e4ce27e0a0d3b09e2e923b8db4306
Mhmd12z <98625548+Mhmd12z@users.noreply.github.com>
2023-07-30 19:32:47 +0300

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,41 @@ window.onload = function () {
         days.innerHTML = time.getDay();
     }, 1000);
 };
+let home=document.querySelector(".main");
+let returnToTop = document.createElement("button");
+let icon = document.createElement("i");
+icon.className = "fa-solid fa-arrow-up fa-bounce";
+returnToTop.appendChild(icon);
+returnToTop.style.cssText = `
+width: 50px;
+height: 70px;
+position: fixed;
+right: 20px;
+bottom: 20px;
+background-color: var(--main-color);
+color: var(--primary-color);
+border: none;
+border-radius: 5px;
+font-size: 30px;
+cursor: pointer;
+opacity: 0;
+z-index: -1;
+transition: var(--main-transition);
+`;
+returnToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+document.body.appendChild(returnToTop);
+window.onscroll=function(){
+if (scrollY > home.offsetTop + 300) {
+    returnToTop.style.zIndex = 100;
+    returnToTop.style.opacity = 1;
+  } else {
+    returnToTop.style.zIndex = -1;
+    returnToTop.style.opacity = 0;
+  }
+}
